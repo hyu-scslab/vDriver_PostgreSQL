9a446dd6eb613dffd1f0a671126089758e5a2eae
Jongbin Kim <mrbin2002@naver.com>
2019-08-22 16:13:12 +0900

    first commit of llt branch

---
+++ src/backend/storage/vcluster/vcluster.c
@@ -0,0 +1,19 @@
+/*-------------------------------------------------------------------------
+ *
+ * vcluster.c
+ *
+ * Version Cluster Implementation
+ *
+ * Portions Copyright (c) 1996-2019, PostgreSQL Global Development Group
+ * Portions Copyright (c) 1994, Regents of the University of California
+ *
+ *
+ * IDENTIFICATION
+ *	  src/backend/storage/vcluster/vcluster.c
+ *
+ *-------------------------------------------------------------------------
+ */
+#include "postgres.h"
+
+#include "storage/vcluster.h"
+
---
+++ src/include/storage/vcluster.h
@@ -0,0 +1,18 @@
+/*-------------------------------------------------------------------------
+ *
+ * vcluster.h
+ *	  Version cluster definitions.
+ *
+ *
+ * Portions Copyright (c) 1996-2019, PostgreSQL Global Development Group
+ * Portions Copyright (c) 1994, Regents of the University of California
+ *
+ * src/include/storage/vcluster.h
+ *
+ *-------------------------------------------------------------------------
+ */
+#ifndef VCLUSTER_H
+#define VCLUSTER_H
+
+
+#endif							/* VCLUSTER_H */
